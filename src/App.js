810bb920652a7d4ac3a163ade1e4d38e3ee573d6
DinethShanG <gimhana.ds@gmail.com>
2021-01-22 02:02:58 +0530

    added the starter code.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Game from "./components/Game";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello Dineth</h1>
+      <Game/>
     </div>
   );
 }
