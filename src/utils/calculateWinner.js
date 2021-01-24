72aad5d7c279c346c2e77e659cb379164deb9cf3
DinethShanG <gimhana.ds@gmail.com>
2021-01-24 10:27:42 +0530

    finalized tic-tac-toe

--- src/utils/calculateWinner.js
+++ src/utils/calculateWinner.js
@@ -1,4 +1,4 @@
-export default function calculateWinner(squares) {
+function calculateWinner(squares) {
     const lines = [
         [0, 1, 2],
         [3, 4, 5],
@@ -18,3 +18,4 @@ export default function calculateWinner(squares) {
     return null;
 }
 
+export default calculateWinner;
